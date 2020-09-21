95a0b2e4ac46a4f3b259eedffdb21934e56ce13d
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-21 01:24:05 +0000

    recursion summation

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if(fromN === toN) { //if the numbers are equal then return this number
+    return fromN;
+  }
+  return sumToOne(toN) - sumToOne(fromN) + fromN;
+};
+function sumToOne(n) {
+  if (n === 0) {
+    return 0; //base case should be 0 if the number less than zero then recursion function should stop
+  }
+  return n + sumToOne(n - 1);
+};
+// console.log(sumToOne(1))
+// console.log(sum(3,4))
 
 module.exports = sum;
